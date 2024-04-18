837520cf51fc7ef0be98ce165f47918f8b2a6c03
wnrrpTdjdy <eric3614@naver.com>
2024-04-18 14:13:09 +0000

    add skeleton

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def even_list(int_:List[int]) -> List[int]:
+    #TODO
+
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    #TODO
+def main():
+    #Example
+    int_list = [1,2,3,4,5,6,7,8,9,10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+if __name__ == "__main__":
+    main()
